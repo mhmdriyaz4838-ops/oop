e5fe0af66698a35ab56b17e29cc94efdd94b77ae
mhmdriyaz4838-ops <mhmdriyaz4838>
2025-11-04 14:20:31 +0530

    alphabetical order

---
+++ JL/src/com/Day1/HW/AlphabeticalOrder.java
@@ -0,0 +1,24 @@
+package com.Day1.HW;
+
+public class AlphabeticalOrder {
+    public static void main(String[] args){
+
+
+
+    char ch1 = 'z';
+    char ch2 = 'a';
+
+            if(ch1<ch2)
+
+        {
+            System.out.println(ch1+","+ch2);
+
+
+}
+            else{
+                System.out.println(ch2+","+ch1);
+            }
+
+
+            }
+}
